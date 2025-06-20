1cd06752b4e1fc4b0d628793479170da8079b2a5
veerababu558 <44125493+veerababu558@users.noreply.github.com>
2025-06-20 13:33:36 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/hello")
 def hello():
-    return {"message": "Hello from FastAPI on Lambda V1 !"}
+    return {"message": "Hello from FastAPI on Lambda V2 !"}
 
 # Lambda handler
 handler = mangum.Mangum(app)
